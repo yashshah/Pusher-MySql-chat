2351c5f1aed0bf96b7ed3252433b122d0f4ca185
Yash Shah <mail@yashshah.com>
2013-07-28 16:14:17 +0000

    Add chat migration

---
+++ db/migrate/20130728160253_add_content_to_chat.rb
@@ -0,0 +1,10 @@
+class AddContentToChat < ActiveRecord::Migration
+  def change
+  	create_table :messages do |t|
+      t.string :content
+      t.integer :user_id
+      
+      t.timestamps
+    end
+  end
+end
