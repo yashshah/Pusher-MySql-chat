d5890dd0a67dc725721411078308c2c4213591fb
Yash Shah <mail@yashshah.com>
2013-07-28 15:47:27 +0000

    Change home page to chat index action

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 RailsRealtime::Application.routes.draw do
+  root :to => "chat#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
